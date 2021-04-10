d209e131cf37ed29451ef00f0db3c5042e7e9245
Teodoro Garcia <tedtke514@gmail.com>
2021-04-10 15:06:28 -0500

    api: added login route

--- deckbox_api/config/routes.rb
+++ deckbox_api/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     resources :deckboxes
   end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/login', to: 'auth#create'
+
+  
 end
