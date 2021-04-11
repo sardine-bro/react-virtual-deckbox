e3fa9c5655c166c6f953ede417b53f683280b4d0
Teodoro Garcia <tedtke514@gmail.com>
2021-04-11 13:55:04 -0500

    fixed bugs in profile component

--- deckbox_client/src/components/Profile.js
+++ deckbox_client/src/components/Profile.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import { profileRequest } from '../services/api'
 
 class Profile extends Component {
+    state = {
+        username: '',
+        deckBoxes: []
+    }
+
     componentDidMount() {
-        profileRequest().then(console.log)
+        profileRequest()
+        .then(res => {
+            if (!res.error) {
+                this.setState({ username: res.username, deckBoxes: res.deckboxes})
+            }
+        })
     }
     render() {
         return (
             <div id="profile">
-                <h1>Your Profile  </h1>
+                {this.state.username ? <h1>{this.state.username}'s Profile</h1> : <h1>Loading...</h1>}
             </div>
         )
     }
